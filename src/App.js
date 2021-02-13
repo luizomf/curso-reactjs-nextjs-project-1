faa93b602b395087f6745af97efb1f8487074d65
Luiz Otávio <luizomf@gmail.com>
2021-02-13 12:45:52 -0300

    Props e organização - parte 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './App.css';
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
   state = {
@@ -33,13 +34,13 @@ class App extends Component {
       <section className="container">
         <div className="posts">
           {posts.map(post => (
-            <div className="post">
-              <img src={post.cover} alt={post.title} />
-              <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
+            <PostCard 
+              key={post.id}
+              title={post.title} 
+              body={post.body}
+              id={post.id}
+              cover={post.cover}
+            />
           ))}
         </div>
       </section>
---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,9 @@
+export const PostCard = ({ title, cover, body, id }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  </div>
+);
