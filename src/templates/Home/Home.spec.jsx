58fa5f52067b381b7a8a232db8ef9621647b2401
Luiz Otávio <luizomf@gmail.com>
2021-03-24 18:43:47 -0300

    Testando o template <Home />

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -51,7 +51,17 @@ describe('<Home />', () => {
     render(<Home />);
     const noMorePosts = screen.getByText('Não existem posts =(');
 
+    expect.assertions(3);
+
     await waitForElementToBeRemoved(noMorePosts);
-    screen.debug();
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
   });
 });
